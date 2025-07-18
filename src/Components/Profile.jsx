406901d927e7501c82436a6d0ff07410273d9cf7
Wilson <fkkr58@durham.ac.uk>
2025-07-18 16:15:28 +0100

    Home page

---
+++ src/Components/Profile.jsx
@@ -0,0 +1,11 @@
+export function Profile({person}) {
+
+    return  (
+        <div className="Profile">
+            <div className="movie-info">
+                <h3>{person.name}</h3>
+                <p>{person.pfp}</p>
+            </div>
+        </div>    
+    );
+}
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,37 @@
+import {Profile} from "../Components/Profile"
+import {useState} from "react"
+
+
+export function Home() {
+    const [searchQuery, setSearchQuery] = useState(""); 
+
+    const profiles = [
+        {id: 1, name: "John Wick", pfp: "o"},
+        {id: 2, name: "Wilson Cheung", pfp: "o"},
+        {id: 3, name: "Xi Nan Shu", pfp: "o"},
+        {id: 4, name: "Stephen Chou", pfp: "o"}
+    ];
+
+    const handleSearch = (e) => {
+        e.preventDefault()
+        alert(searchQuery)
+        setSearchQuery("")
+    };
+
+    return (
+    
+    
+    <div className="home">
+        <form onSubmit={handleSearch} className="search-form">
+            <input type = "text" placeholder="Search" className="search-input" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)}/>
+            <button type="submit" className="search-button">Search</button>      
+        </form>
+
+          
+        <div className="profile-grid">
+            {profiles.map(person => (
+                <Profile person={person} key={person.id}/>
+            ))}
+        </div>
+    </div>
+)}
