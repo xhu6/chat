d888305dc32ba39da824e436a9abab1631f5ee72
Wilson <fkkr58@durham.ac.uk>
2025-07-18 21:43:25 +0100

    better looking version

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,14 +1,15 @@
 import { Profile } from "../Components/Profile";
 import { useState } from "react";
+import { Link } from "react-router-dom"
 
 export function Home() {
   const [searchQuery, setSearchQuery] = useState("");
 
   const profiles = [
-    { id: 1, name: "John Wick", pfp: "o" },
-    { id: 2, name: "Wilson Cheung", pfp: "o" },
-    { id: 3, name: "Xi Nan Shu", pfp: "o" },
-    { id: 4, name: "Stephen Chou", pfp: "o" },
+    { id: 1, name: "John Wick", pfp: "/vite.svg" },
+    { id: 2, name: "Wilson Cheung", pfp: "/vite.svg" },
+    { id: 3, name: "Xi Nan Shu", pfp: "/vite.svg" },
+    { id: 4, name: "Stephen Chou", pfp: "/vite.svg" },
   ];
 
   const handleSearch = (e) => {
@@ -18,20 +19,22 @@ export function Home() {
   };
 
   return (
-    <div className="home">
-      <form onSubmit={handleSearch} className="search-form">
-        <input
-          type="text"
-          placeholder="Search"
-          className="search-input"
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-        />
-        <button type="submit" className="search-button">
-          Search
-        </button>
-      </form>
-
+    <div className="home flex h-screen flex-col bg-slate-900">
+        <div className="flex flex-none gap-4 bg-slate-700 p-4 justify-center">
+          <form onSubmit={handleSearch} className="search-form flex gap-2 border">
+            <input
+            type="text"
+            placeholder="Search"
+            className="search-input"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            />
+            <button type="submit" className="search-button">
+            Search
+            </button>
+            <Link to={'/Settings'} className="nav-link text-white text-xl font-medium hover:text-slate-200">...</Link>
+          </form>
+        </div>
       <div className="profile-grid">
         {profiles.map((person) => (
           <Profile person={person} key={person.id} />  
--- src/Pages/Settings.jsx
+++ src/Pages/Settings.jsx
@@ -1,13 +1,15 @@
 export function Settings() {
     return (
-        <div>
-            <h1>Name</h1>
-            <img
-            className="h-12 w-12"
-            src="/vite.svg"
-            alt="John Wick's profile picture."
-            />
-            <p>Bio</p>
+        <div className="home flex h-screen flex-col bg-slate-900">
+            <div className="bg-slate-800 flex flex-col items-center text-white text-xl font-medium text-center gap-2 ">
+                <h1>Name</h1>
+                <img
+                className="h-12 w-12"
+                src="/vite.svg"
+                alt="John Wick's profile picture."
+                />
+                <p>Bio</p>
+            </div>
         </div>
     );
 }
