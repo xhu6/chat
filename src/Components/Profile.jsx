e3db4975ca5945dd2ebfc3b00070e7e14a04bb27
xhu6 <143360206+xhu6@users.noreply.github.com>
2025-07-18 21:19:49 +0000

    Allow whole profile entry to be clickable

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -2,16 +2,15 @@ import { Link } from "react-router";
 
 export function Profile({ person }) {
   return (
-    <div className="Profile m-2 rounded-lg border border-slate-700 bg-slate-800 p-4 transition-colors hover:bg-slate-700">
-      <div className="Person-info flex items-center gap-2 text-center">
-        <img src={person.pfp}></img>
-        <Link
-          to={`/chat/${person.name}`}
-          className="nav-link text-2xl font-medium text-white hover:text-slate-200"
-        >
-          {person.name}
-        </Link>
+    <Link to={`/chat/${person.name}`}>
+      <div className="Profile m-2 rounded-lg border border-slate-700 bg-slate-800 p-4 transition-colors hover:bg-slate-700">
+        <div className="Person-info flex items-center gap-2 text-center">
+          <img src={person.pfp}></img>
+          <p className="nav-link text-2xl font-medium text-white hover:text-slate-200">
+            {person.name}
+          </p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
