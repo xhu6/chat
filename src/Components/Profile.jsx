ec9e50ca532810a001d91bae5f56b9accaab3d9a
xhu6 <143360206+xhu6@users.noreply.github.com>
2025-07-18 15:55:07 +0000

    Run formatter

--- src/Components/Profile.jsx
+++ src/Components/Profile.jsx
@@ -1,11 +1,10 @@
-export function Profile({person}) {
-
-    return  (
-        <div className="Profile">
-            <div className="movie-info">
-                <h3>{person.name}</h3>
-                <p>{person.pfp}</p>
-            </div>
-        </div>    
-    );
+export function Profile({ person }) {
+  return (
+    <div className="Profile">
+      <div className="movie-info">
+        <h3>{person.name}</h3>
+        <p>{person.pfp}</p>
+      </div>
+    </div>
+  );
 }
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,37 +1,42 @@
-import {Profile} from "../Components/Profile"
-import {useState} from "react"
-
+import { Profile } from "../Components/Profile";
+import { useState } from "react";
 
 export function Home() {
-    const [searchQuery, setSearchQuery] = useState(""); 
+  const [searchQuery, setSearchQuery] = useState("");
 
-    const profiles = [
-        {id: 1, name: "John Wick", pfp: "o"},
-        {id: 2, name: "Wilson Cheung", pfp: "o"},
-        {id: 3, name: "Xi Nan Shu", pfp: "o"},
-        {id: 4, name: "Stephen Chou", pfp: "o"}
-    ];
+  const profiles = [
+    { id: 1, name: "John Wick", pfp: "o" },
+    { id: 2, name: "Wilson Cheung", pfp: "o" },
+    { id: 3, name: "Xi Nan Shu", pfp: "o" },
+    { id: 4, name: "Stephen Chou", pfp: "o" },
+  ];
 
-    const handleSearch = (e) => {
-        e.preventDefault()
-        alert(searchQuery)
-        setSearchQuery("")
-    };
+  const handleSearch = (e) => {
+    e.preventDefault();
+    alert(searchQuery);
+    setSearchQuery("");
+  };
 
-    return (
-    
-    
+  return (
     <div className="home">
-        <form onSubmit={handleSearch} className="search-form">
-            <input type = "text" placeholder="Search" className="search-input" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)}/>
-            <button type="submit" className="search-button">Search</button>      
-        </form>
+      <form onSubmit={handleSearch} className="search-form">
+        <input
+          type="text"
+          placeholder="Search"
+          className="search-input"
+          value={searchQuery}
+          onChange={(e) => setSearchQuery(e.target.value)}
+        />
+        <button type="submit" className="search-button">
+          Search
+        </button>
+      </form>
 
-          
-        <div className="profile-grid">
-            {profiles.map(person => (
-                <Profile person={person} key={person.id}/>
-            ))}
-        </div>
+      <div className="profile-grid">
+        {profiles.map((person) => (
+          <Profile person={person} key={person.id} />
+        ))}
+      </div>
     </div>
-)}
+  );
+}
