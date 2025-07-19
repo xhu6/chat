187a221b2c99931953e84ecec55c6950128b278d
Wilson <fkkr58@durham.ac.uk>
2025-07-19 13:13:39 +0100

    starting the database

---
+++ src/Database/statements.sql
@@ -0,0 +1,10 @@
+-- CREATE TABLE users (
+--     id INTEGER PRIMARY KEY AUTOINCREMENT,
+--     name TEXT NOT NULL,
+--     age INTEGER
+-- );
+
+-- INSERT INTO users(id, name, age)
+-- VALUES (1, "Wilson", 19)
+
+SELECT * FROM users
