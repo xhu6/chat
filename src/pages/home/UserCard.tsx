c60225eedc94ffa6623f97d603c5d0261978f0bb
xhu6 <143360206+xhu6@users.noreply.github.com>
2025-08-17 12:57:39 +0000

    Use Roboto

--- src/pages/home/UserCard.tsx
+++ src/pages/home/UserCard.tsx
@@ -8,7 +8,7 @@ export function UserCard({ user }: { user: User }) {
       <div className="rounded-lg border border-slate-700 bg-slate-800 p-4 transition-colors hover:bg-slate-700 hover:text-white">
         <div className="flex items-center gap-2 text-center">
           <img className="h-10 w-10" src={user.pfp ?? "/tauri.svg"}></img>
-          <p className="text-2xl font-medium text-gray-200">{user.name}</p>
+          <p className="text-2xl text-gray-200">{user.name}</p>
         </div>
       </div>
     </Link>
